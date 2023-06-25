6f1f08a03c833786a2a431a3b00ffd6950d3e9d9
Muhammad Zubair Shabbir <bsef20a534@pucit.edu.pk>
2023-06-25 09:19:51 +0500

    all commist

---
+++ MadrasazApplication/app/src/main/java/com/example/madrasazapplication/InsertDailyTask.java
@@ -0,0 +1,43 @@
+package com.example.madrasazapplication;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class InsertDailyTask extends AppCompatActivity {
+
+    // temp
+    TaskDBHelper db;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_daily_task);
+
+        db = new TaskDBHelper(this);
+        Button insertStudentTask = findViewById(R.id.insert_student_task_btn);
+        TextView sabaqView = findViewById(R.id.edit_student_record_sabaq);
+        TextView sabaqiView = findViewById(R.id.edit_student_record_sabaqi);
+        TextView manzalView = findViewById(R.id.edit_student_record_manzal);
+        TextView rollNoView = findViewById(R.id.edit_student_record_rollno);
+        insertStudentTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String rollNo = rollNoView.getText().toString();
+                String sabaq = sabaqView.getText().toString();
+                String sabaqi = sabaqiView.getText().toString();
+                String manzal = manzalView.getText().toString();
+                String currentDate = db.getCurrentDate();
+                Task task = new Task(rollNo, sabaq, sabaqi, manzal, currentDate);
+                db.insertTask(task);
+
+                Intent intent = new Intent(InsertDailyTask.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
